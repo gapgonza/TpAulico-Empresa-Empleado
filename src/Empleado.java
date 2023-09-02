3c4cdec7a46308865a0f8876990550b972680540
ALAN PEÑIÑOREY <gapgonza@gmail.com>
2023-09-02 17:58:54 -0300

    Initial commit

---
+++ src/Empleado.java
@@ -0,0 +1,59 @@
+public class Empleado {
+    private int dni;
+    private String nombreApellido;
+    private String categoria;
+    private double sueldo;
+    private Empresa empresa;
+
+    public Empleado() {
+    }
+
+    public Empleado(int dni, String nombreApellido, String categoria, double sueldo, Empresa empresa) {
+        this.dni = dni;
+        this.nombreApellido = nombreApellido;
+        this.categoria = categoria;
+        this.sueldo = sueldo;
+        this.empresa = empresa;
+    }
+    
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombreApellido() {
+        return nombreApellido;
+    }
+
+    public void setNombreApellido(String nombreApellido) {
+        this.nombreApellido = nombreApellido;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+    
+}
---
+++ src/Empresa.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+
+public class Empresa {
+    private String razonSocial;
+    private int cuit;
+    private ArrayList<Empleado> empleados;
+
+    public Empresa(String razonSocial, int cuit) {
+        this.razonSocial = razonSocial;
+        this.cuit = cuit;
+        this.empleados = new ArrayList<Empleado>();
+    }
+     public void agregarEmpleado(Empleado e) {
+         this.empleados.add(e);
+    }
+
+    public String getRazonSocial() {
+        return razonSocial;
+    }
+
+    public void setRazonSocial(String razonSocial) {
+        this.razonSocial = razonSocial;
+    }
+
+    public int getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(int cuit) {
+        this.cuit = cuit;
+    }
+    
+    public void mostrarEmpleado() {
+    }
+}
